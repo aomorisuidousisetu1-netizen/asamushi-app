e923e0f4f142d8a907c81077bddb940278bb508a
aomorisuidousisetu1-netizen <aomori.suidou.sisetu1@gmail.com>
2026-02-04 15:07:33 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.GOOGLE_SCRIPT_URL': JSON.stringify(process.env.GOOGLE_SCRIPT_URL),
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.GOOGLE_SCRIPT_URL': JSON.stringify(process.env.GOOGLE_SCRIPT_URL || ''),
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   }
 });
